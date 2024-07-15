5505a0663b1f6ef77e34f96b795e8b7730d26f4a
MacaPazAC <mac.avendano@duocuc.cl>
2024-07-15 03:30:27 -0400

    1.7
    Cambios generales en modelo CartItem en Form.py

--- ProyectoVerdor/productos/forms.py
+++ ProyectoVerdor/productos/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Producto
+from .models import Producto,CartItem
 
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['id_prod', 'nombre', 'id_cate', 'precio', 'descripcion', 'stock', 'imagen']
+
+class CartItemForm(forms.ModelForm):
+    class Meta:
+        model = CartItem
+        fields = ['name', 'price', 'quantity']
